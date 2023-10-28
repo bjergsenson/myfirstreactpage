5a39f4011afde24f3bd16b856936162eccf9802b
Manuel Alejandro Londoño Cadavid <malondonoc@gmail.com>
2023-10-28 13:18:34 -0500

    Update App.js to datatable format

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,46 +1,45 @@
 import axios from 'axios';
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
-const App = (props) => {
-  //define the state and the setter
-  const [APIlist, setAPIlist] = useState();
+const App = (props)=> {
+    //define the state and the setter
+    const [APIlist, setAPIlist] = useState();
 
-  //On load invoke method to generate list
-  useEffect(() => {
-    let url = "https://api.publicapis.org/entries?category=Animals";
-    axios({
-      method: 'get',
-      url: url,
-      responseType: 'json'
-    }).then(resp => {
-      let listOfEntries = resp.data.entries;
-      let listOfEntriesAsArray = Object.entries(listOfEntries);
-      let i = 1;
-      let entryDetails = listOfEntriesAsArray.map((entryDetail) => {
-        return (
-          <li key={i++}>
-            <a href={entryDetail[1]["Link"]} target="_blank" rel="noreferrer">{entryDetail[1]["API"]}</a>
-          </li>
-        )
-      })
-      setAPIlist(entryDetails);
-    })
-      .catch(err => {
-        console.log(err.toString())
-      });
-  }, []);
+    //On load invoke method to genrate list
+    useEffect(() => {
+        let url = "https://api.publicapis.org/entries?category=Animals";
+        axios({
+          method: 'get',
+          url: url,
+          responseType: 'json'
+        }).then(resp => {
+            let listOfEntries = resp.data.entries;
+            let listOfEntriesAsArray = Object.entries(listOfEntries);
+            let i=1;
+            let entryDetails = listOfEntriesAsArray.map((entryDetail)=>{
+              return(
+                <tr key={i++}>
+                  <td key={i++} style={{color: "red",border: "1px solid black"}}>					{entryDetail[1]["API"]} </td>
+                  <td key={i++} style={{color: "red",border: "1px solid black"}}> 					{entryDetail[1]["Link"]} </td>
+                </tr>)
+            })
+            setAPIlist(entryDetails);
+          })
+        .catch(err => {
+            console.log(err.toString())
+        });
+    }, []);
 
+    const colorStyle = { color:props['color'], fontSize:props['size']}
 
-  const colorStyle = { color: props['color'], fontSize: props['size'] }
+    return (
+        <div>
+            <h2>APIs List</h2>
+            <br/>
 
-  return (
-    <div>
-      <h2>APIs List</h2>
-      <br />
-
-      <div style={colorStyle}><ul>{APIlist}</ul></div>
-    </div>
-  );
+            <div style={colorStyle}><ul>{APIlist}</ul></div>
+        </div>
+);
 }
 
 export default App;
