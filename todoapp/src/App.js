d33227c782ca4643edc054411f0612953d87c848
Manuel Alejandro Londoño Cadavid <malondonoc@gmail.com>
2023-10-28 13:07:35 -0500

    Update App.js

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(props) {
+  const colorStyle = {
+    color: props.color,
+    fontSize: props.size
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My First React Application
-        </a>
-      </header>
+    <div>
+      <div style={colorStyle}>
+        Hello World!
+      </div>
     </div>
   );
 }
